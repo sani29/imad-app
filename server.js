791deb0bd135c57602f7b3c6146ed1c4c99f0cbd
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-08-18 00:47:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
