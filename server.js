2296ac47d928dbb8df3582aead4b2c59df23025e
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-08-18 01:06:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res){
     //articleName == article-one
     //articles[articleName] == content object for article-one
-    var articleName = req.params.articleName;
+    var articleName = req.params.articleName; //property given by express framework
     res.send(createTemplate(articles[articleName]));
 });
 
