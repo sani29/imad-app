a9fdc83144f1f6be9542294d2106380b2dca0f17
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-08-19 14:36:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 
 element.innerHTML="New Value";
+
+//move the image
+var img= document.getElementById('Madi');
+img.onclick = function(){
+    img.style.marginLeft = "100px";
+};
+
