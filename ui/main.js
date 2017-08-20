c62b0d2d114c2a9b17c80e7f08cfb7224535f9b6
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-08-20 18:23:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,9 +23,23 @@ button.onclick = function(){
      request.send(null);
 };
 
+//submit name
 
-
-
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    //make request to the serverand send name
+    
+    //capture a list of names and render the list
+    var names = ["name1","name2","name3","name4"];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list += "<li>" + names[i] + "</li>";
+        }
+    var ul = document.getElementById("nameList");
+    ul.innerHTML = list;
+}
 
 
 
