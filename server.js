750f6264172aa5c1de85717aba15cb3c40a84639
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-08-20 18:37:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get("/submit-name/:name",function(req,res){
     //get the name from the request
     var name = req.params.name;
     
-    name.push(name);
+    names.push(name);
     //JSON javascript object notation
     res.send(JSON.stringify(names)); //1000
 });
