196e9a4a3475628dd8b9fec133cdde0737046736
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-08-23 00:10:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function(){
 
 //submit name
 
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
     
@@ -52,6 +51,8 @@ submit.onclick = function(){
          //not done yet
      };
      //make the request
+     var nameInput = document.getElementById("name");
+     var name = nameInput.value;
      request.open("GET","http://pushpsanidhya29.imad.hasura-app.io/submit-name?name=" + name,true);
      request.send(null);
     
