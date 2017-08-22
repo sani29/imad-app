15205db2c964d0262f16f09794f263bee83498ba
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-08-23 00:04:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,16 +29,33 @@ var nameInput = document.getElementById("name");
 var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
-    //make request to the serverand send name
     
-    //capture a list of names and render the list
-    var names = ["name1","name2","name3","name4"];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list += "<li>" + names[i] + "</li>";
-        }
-    var ul = document.getElementById("nameList");
-    ul.innerHTML = list;
+    //create a request object
+     var request = new XMLHttpRequest();
+     
+    //capture the response and store in a variable
+     request.onreadystatechange = function(){
+         if(request.readyState === XMLHttpRequest.DONE){
+             //take some action
+             if(request.status === 200){
+              //capture a list of names and render the list
+                  var names = request.responseText;
+                  names = JSON.parse(names);
+                  var list = '';
+                for(var i=0;i<names.length;i++){
+                     list += "<li>" + names[i] + "</li>";
+                 }
+                var ul = document.getElementById("nameList");
+                ul.innerHTML = list;
+             }
+         }
+         //not done yet
+     };
+     //make the request
+     request.open("GET","http://pushpsanidhya29.imad.hasura-app.io/counter",true);
+     request.send(null);
+    
+    
 };
 
 
