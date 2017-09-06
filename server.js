487bd9559865ce05ab675b721a728d19f184e56c
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-09-06 15:04:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('/articles/:articleName',function(req,res){
     pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, fuction(err,result) {
        if(err){
             res.status(500).send(err.toString());
-        }else{
+        } else{
             if( result.rows.length === 0){
                 res.status(404).send('Article not found');
             } else{
