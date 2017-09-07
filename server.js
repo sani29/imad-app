089251fab12b3d3f4f68d6edb462cff533e92753
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-09-08 00:43:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/', function (req, res) {
 });
 
 var pool= new Pool(config);
-app.get('/test', function(req,res){
+app.get('/test-db', function(req,res){
     //make a select request
     //return a response with the results
     pool.query('SELECT * FROM test',function(err,result){
