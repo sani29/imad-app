61539f91686e7bb55b56a69b9232f8fb546ef9c6
Sanidhya Pushp <pushp.sanidhya29@gmail.com>
2017-09-08 15:59:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config={
     user:'pushpsanidhya29',
@@ -52,6 +53,17 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input,salt){
+    //how do we create hash
+    var hashed = crypto.pbkdf2Sync(input,salt,10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input' , function(req,res){
+    var hashedString = has(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 var pool= new Pool(config);
